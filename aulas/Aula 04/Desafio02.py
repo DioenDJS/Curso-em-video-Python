77a4e345784c6d4ff41facf4ea0f6562fc388913
Diovane Pacheti <negociospacheti@gmail.com>
2023-06-01 22:23:16 -0300

    :tada: Iniciando os estudos

---
+++ aulas/Aula 04/Desafio02.py
@@ -0,0 +1,11 @@
+'''
+Crie um script python que leia o dia, mês e ano de nascimento de uma pessoa
+e mostre uma mensagem com a data formatada.
+'''
+
+print(' ============= DESAFIO 02 =============')
+dia = input('Qual dia vc nasceu ? ')
+mes = input('Em mês em que vc nasceu ? ')
+ano = input('Qual o ano em que vc nasceu ? ')
+
+print('você nasceu no dia ', dia, ' de ', mes,' de ',ano)
---
+++ aulas/Aula 04/Desafio03.py
@@ -0,0 +1,10 @@
+'''
+Crie um script python que leia dois numeros e tente mostrar a soma entre eles.
+'''
+
+print(' ============= DESAFIO 03 =============')
+numero1 = int(input('Digite o primeiro numero : '))
+numero2 = int(input('Digite o segundo numero : '))
+soma = (numero1 + numero2)
+
+print('A soma é : ', soma)
