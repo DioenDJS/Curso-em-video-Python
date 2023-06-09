1747634d2189150467f6ccf9b676e60ddd2af031
Diovane Pacheti <negociospacheti@gmail.com>
2023-06-08 23:05:10 -0300

    :recycle: Refactor code

--- exercicios/ex012.py
+++ exercicios/ex012.py
@@ -4,8 +4,8 @@ Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com
 
 preco = float(input('Digite o preço do produto : '))
 
-precoDesconto = (5 / 100) * preco
+precoDesconto = (preco * 5) / 100
 
 novoPreco = preco - precoDesconto
 
-print('{}R$ com 5% de desconto tem um desconto de {}R$ e vai ficar : {}R$'.format(preco, precoDesconto, novoPreco))
+print('{}R$ com 5% de desconto tem um desconto de {:.2f}R$ e vai ficar : {:.2f}R$'.format(preco, precoDesconto, novoPreco))
