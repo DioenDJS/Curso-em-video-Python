351d1a5ab88671ed9f42b4824b8dd9b42f6446bc
Diovane Pacheti <negociospacheti@gmail.com>
2023-06-08 21:36:34 -0300

    :art: Fix format

--- exercicios/ex006.py
+++ exercicios/ex006.py
@@ -7,4 +7,4 @@ num = int(input('Digite um número : '))
 numDobro = num * 2
 numTriplo = num * 3
 numRaiz = num ** (1/2)
-print('O dobro do valor {} é : {} o triplo é : {} e a raiz quadrada é : {} '.format(num, numDobro, numTriplo, numRaiz))
+print('O dobro do valor {} é : {} o triplo é : {} e a raiz quadrada é : {:.3} '.format(num, numDobro, numTriplo, numRaiz))
