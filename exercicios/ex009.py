02f9f1daf0c9105bd45edc5d6df3fa6e2d114c98
Diovane Pacheti <negociospacheti@gmail.com>
2023-06-08 22:29:12 -0300

    :art: Formatting print output

--- exercicios/ex009.py
+++ exercicios/ex009.py
@@ -5,4 +5,9 @@ Faça um programa que leia um número inteiro qualquer e mostre na tela a sua ta
 num = int(input('Digite um numero : '))
 
 for i in range(11):
-        print('{} x {} = {}'.format(num, i, (i*num)))
+  if i < 10:
+    print('{} x {:2} = {}'.format(num, i, (i * num)))
+  else:
+    print('{} x {} = {}'.format(num, i, (i * num)))
+
+
