2e95b74e0ae575d9c56a1eca9a75648b9e1b5999
Diovane Pacheti <negociospacheti@gmail.com>
2023-06-08 21:59:35 -0300

    :pencil2: Fix format

--- exercicios/ex007.py
+++ exercicios/ex007.py
@@ -1,9 +1,11 @@
 """
 Desenvolva um programa que leia as duas notas de um aluno, colcule e mostre a sua média.
 """
+import math
+
 notaUm = float(input('Digite a primeira nota : '))
 notaDois = float(input('Digite a segunda nota : '))
 
 media = (notaUm + notaDois) / 2
 
-print('A média entre {} e {} é : {}'.format(notaUm, notaDois, media))
+print('A média entre {} e {} é : {:.1f}'.format(notaUm, notaDois, media))
