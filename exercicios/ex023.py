9bd8efb9cc58e6cc407c80f4603432d69af3ebc9
Diovane Pacheti <negociospacheti@gmail.com>
2023-06-17 14:23:54 -0300

    :bug: Fix ex023

--- exercicios/ex023.py
+++ exercicios/ex023.py
@@ -11,11 +11,17 @@ milhar:1
 """
 print('Digite um número ate 4 digitos ex: 9999 ')
 num = int(input('Digite o número : '))
-numString = str(num)
+# numString = str(num)
 
-unidade = numString[3]
-dezena = numString[2]
-centena = numString[1]
-milhar = numString[0]
+# Refatorando com a solução do Gustavo Guanabara
+unidade = num // 1 % 10
+dezena = num // 10 % 10
+centena = num // 100 % 10
+milhar = num // 1000 % 10
+
+# unidade = numString[3]
+# dezena = numString[2]
+# centena = numString[1]
+# milhar = numString[0]
 
 print('unidade:{} \ndezena:{} \ncentena:{} \nmilhar:{}'.format(unidade, dezena, centena, milhar))
